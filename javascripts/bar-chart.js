5c80e24ee978d1b85af578624765d7090fba5db3
Brendan-funk <brendanzfunk@gmail.com>
2022-02-02 23:57:18 +0000

    Added style.css for later and started writing js function

--- javascripts/bar-chart.js
+++ javascripts/bar-chart.js
@@ -1,3 +1,9 @@
 const drawBarChart = function(data, options, element){
-
+  let max = 0;
+  for(let i = 0; i < data.length; i++){
+    if(data[i]>max){
+      max = data[i];
+    }
+  }
+  const scale = Math.floor(max/data.length);
 };
